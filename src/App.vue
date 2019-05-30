b757838f603713d088ca90005d4cc5bc34cdf5bf
bunzuk1991 <bunzuk1991@gmail.com>
2019-05-30 23:59:24 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
   <div id="root">
-    <div id="nav">
-    </div>
-    <v-app>
-      <v-content>
-        <router-view/>
-      </v-content>
-    </v-app>
+      <v-app id="app-root" class="grey lighten-2">
+        <app-header primary dark></app-header>
+        <v-content></v-content>
+      </v-app>
   </div>
 </template>
+
+<script>
+import AppHeader from '@/components/AppHeader'
+export default {
+  computed: {
+  },
+  data () {
+    return {
+      name: '1'
+    }
+  },
+  methods: {
+  },
+  components: {
+    AppHeader
+  }
+}
+</script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="app-header">
+    <v-toolbar>
+      <v-toolbar-title>
+        <span class="font-weight-regular subheading text-uppercase" >Реєстри прийнятих оплат </span>
+        <span class="ml-1 subheading text-uppercase font-weight-black"> по ситемі EPS</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn
+          flat
+          v-for="(item, i) in menuItems"
+          :key="`menu-item-${i}`"
+        >
+          <v-icon left>{{ item.icon }}</v-icon>
+          {{ item.name }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+let menuList = [
+  { name: 'menu1', icon: 'person' },
+  { name: 'menu2', icon: 'person' },
+  { name: 'menu3', icon: 'person' }
+]
+export default {
+  name: 'AppHeader',
+  computed: {
+    menuItems () {
+      return menuList
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
