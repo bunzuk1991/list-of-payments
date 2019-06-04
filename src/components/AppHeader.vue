cb07ef5b813e851271944fc5bd03ffd10987949c
bunzuk1991 <bunzuk1991@gmail.com>
2019-06-04 23:46:19 +0300

    first commit

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,6 +6,17 @@
           <span class="ml-1 subheading text-uppercase font-weight-black red--text text--darken-2"> по ситемі EPS</span>
         </v-toolbar-title>
         <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <modal v-model="loginShowModal"></modal>
+          <v-btn
+            flat
+            @click="loginShowModal = !loginShowModal"
+          >
+            <v-icon left color="red darken-2">person</v-icon>
+            Увійти
+          </v-btn>
+          <v-divider vertical></v-divider>
+        </v-toolbar-items>
         <v-toolbar-items v-for="(item, i) in menuItems" :key="`menu-item-${i}`">
           <v-divider vertical v-if="i === 0"></v-divider>
           <v-btn
@@ -23,6 +34,7 @@
 </template>
 
 <script>
+import Modal from '@/components/Modal'
 let menuList = [
   { name: 'Історія завантежень', icon: 'history', route: '/history' },
   { name: 'Профіль', icon: 'person', route: '/profile' },
@@ -31,12 +43,22 @@ let menuList = [
 ]
 export default {
   name: 'AppHeader',
+  components: {
+    Modal
+  },
+  data () {
+    return {
+      loginShowModal: false
+    }
+  },
   computed: {
     menuItems () {
       return menuList
-    },
-    data () {
-      return {}
+    }
+  },
+  methods: {
+    testFunc () {
+      this.loginShowModal = !this.loginShowModal
     }
   }
 }
---
+++ src/components/Modal.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-dialog
+      v-model="showDialog"
+      max-width="700px">
+      <v-card>
+        <v-card-title>
+          <h2>TEST</h2>
+        </v-card-title>
+      </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  props: ['dialogVisible'],
+  data () {
+    return {
+      showDialog: null
+    }
+  },
+  model: {
+    prop: 'dialogVisible',
+    event: 'change'
+  },
+  watch: {
+    dialogVisible () {
+      this.showDialog = this.dialogVisible
+    },
+    showDialog () {
+      if (this.showDialog === false && this.dialogVisible === true) {
+        this.$emit('change', false)
+      }
+    }
+  },
+  created: function () {
+    this.showDialog = this.dialogVisible
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
