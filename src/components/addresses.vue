7528bae4d84d3afc460c589caf558f5173a1bd5d
bunzuk1991 <bunzuk1991@gmail.com>
2019-06-27 01:11:27 +0300

    modal confirm component

--- src/components/addresses.vue
+++ src/components/addresses.vue
@@ -30,6 +30,77 @@
           </v-flex>
         </v-layout>
     </v-card>
+      <v-card>
+        <v-card-text>
+          <v-layout align-center justify-space-between row wrap>
+            <v-flex xs12 md8>
+              <div>
+                <h3>Тернопільска обл, Кремеенецький р-н, м.Кременець, вул. Березина 41 кв. 1</h3>
+              </div>
+            </v-flex>
+            <v-flex shrink>
+                <v-btn>Додати послугу</v-btn>
+                <v-btn class="btn-image-only">
+                  <v-icon dark>add</v-icon>
+                </v-btn>
+            </v-flex>
+          </v-layout>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-text>
+          <h3 class="font-weight-regular text-uppercase">Комунальні платежі "Єдина квитанція"</h3>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-text class="px-5">
+          <v-layout align-center justify-space-around row>
+            <v-flex md8 offset-md-2>
+              <v-layout row wrap offset-md-1 class="pa-3 text-md-center grey">
+                <v-flex md1>Іконка</v-flex>
+                <v-flex md3>Послуга</v-flex>
+                <v-flex md3>Особовий рахунок</v-flex>
+                <v-flex md2>Місяць</v-flex>
+                <v-flex md3>Сума</v-flex>
+              </v-layout>
+              <v-layout row wrap offset-md-1 class="pa-3 text-md-center grey lighten-2">
+                <v-flex md1><div>Icob</div></v-flex>
+                <v-flex md3><div>Водопостачанн</div></v-flex>
+                <v-flex md3><div>544545</div></v-flex>
+                <v-flex md2><div>Травень 2019</div></v-flex>
+                <v-flex md3><div>0.00</div></v-flex>
+              </v-layout>
+              <v-layout row wrap offset-md-1 class="pa-3 text-md-center grey lighten-2">
+                <v-flex md1><div>Icob</div></v-flex>
+                <v-flex md3><div>Водопостачанн</div></v-flex>
+                <v-flex md3><div>544545</div></v-flex>
+                <v-flex md2><div>Травень 2019</div></v-flex>
+                <v-flex md3><div>0.00</div></v-flex>
+              </v-layout>
+              <v-layout row wrap offset-md-1 class="pa-3 text-md-center grey lighten-2">
+                <v-flex md1><div>Icob</div></v-flex>
+                <v-flex md3><div>Водопостачанн</div></v-flex>
+                <v-flex md3><div>544545</div></v-flex>
+                <v-flex md2><div>Травень 2019</div></v-flex>
+                <v-flex md3><div>0.00</div></v-flex>
+              </v-layout>
+              <v-layout row wrap offset-md-1 class="pa-3 text-md-center grey lighten-2">
+                <v-flex md1><div>Icob</div></v-flex>
+                <v-flex md3><div>Водопостачанн</div></v-flex>
+                <v-flex md3><div>544545</div></v-flex>
+                <v-flex md2><div>Травень 2019</div></v-flex>
+                <v-flex md3><div>0.00</div></v-flex>
+              </v-layout>
+              <v-layout row wrap offset-md-1 class="pa-3 text-md-center grey lighten-2">
+                <v-flex md1><div>Icob</div></v-flex>
+                <v-flex md3><div>Водопостачанн Водопостачанн Водопостачанн Водопостачанн Водопостачанн</div></v-flex>
+                <v-flex md3><div>544545</div></v-flex>
+                <v-flex md2><div>Травень 2019</div></v-flex>
+                <v-flex md3><div>0.00</div></v-flex>
+              </v-layout>
+
+            </v-flex>
+          </v-layout>
+        </v-card-text>
+      </v-card>
     </v-container>
   </div>
 </template>
@@ -50,4 +121,8 @@ export default {
 </script>
 
 <style scoped>
+  .btn-image-only {
+    /*width: 80px;*/
+    min-width: 54px;
+  }
 </style>
